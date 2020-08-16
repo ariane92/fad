3f293ee12ec495a6cf9211edbf7d61d3c81c5bcc
Ariane Mateus <ariane.ccomp@gmail.com>
2020-08-16 19:28:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return <NavigationContainer>{/* coment */}</NavigationContainer>;
+};
+
+export default App;
